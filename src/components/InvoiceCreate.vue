6a829f681138f8bce5279fc67b41c3bf25da0b8c
simba <simbakriss@gmail.com>
2024-06-19 22:15:50 +0300

    fields set up

--- src/components/InvoiceCreate.vue
+++ src/components/InvoiceCreate.vue
@@ -1,4 +1,29 @@
-
 <template>
 <h2>Create an Invoice</h2>
+<div class="create-invoice-form">
+    <form  method="post">
+         
+        <fieldset class="form-group">
+            <label for="status">Clients</label>
+            <select >
+                <option>Chris Simba - simbakriss@gmail.com </option>
+                <option>Paid</option>
+            </select>
+          </fieldset>
+
+        <fieldset class="form-group">
+            <label for="due_date">Due Date:</label>
+            <input type="date" name="due_date" id="due_date">
+        </fieldset>
+
+          <fieldset class="form-group">
+            <label for="status">Status</label>
+            <select >
+                <option>Unpaid</option>
+                <option>Paid</option>
+            </select>
+          </fieldset>
+    </form>
+</div>
+
 </template>
